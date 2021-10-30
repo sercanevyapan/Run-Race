501b0e6798c824adecbc7236855a8e4b0498c3e1
Sercan Evyapan <sevyapan@gmail.com>
2021-10-30 15:14:49 +0300

    Add Player Jump

--- Assets/Scripts/Players Scripts/PlayerController.cs
+++ Assets/Scripts/Players Scripts/PlayerController.cs
@@ -23,12 +23,21 @@ public class PlayerController : MonoBehaviour
 
         if (charController.isGrounded)
         {
-
+            verticalVelocity = 0;
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            gravity = 30;
+            verticalVelocity -= gravity * Time.deltaTime; 
         }
 
         move.Normalize();
         move *= speed;
-      
+        move.y = verticalVelocity;
         charController.Move(move * Time.deltaTime);
     }
 }
